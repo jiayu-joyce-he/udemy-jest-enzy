e54985437afbca59079e3902ec27bf7d7d4a861b
jiayu-joyce-he <jiayuhe@alumni.stanford.edu>
2020-03-16 09:01:48 -0700

    add first few tests

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @format */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { render } from 'enzyme'
+
+class App extends Component {
+    render() {
+        return (
+            <div className='App' data-test='component-app'>
+                <h1 data-test='counter-display'>The counter is currently: </h1>
+                <button data-test='increment-button'>+</button>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,55 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+/** @format */
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import React from 'react'
+import Enzyme, { shallow, ShallowWrapper } from 'enzyme'
+import EnzymeAdapter from 'enzyme-adapter-react-16'
+import { render } from '@testing-library/react'
+
+import App from './App'
+
+Enzyme.configure({ adapter: new EnzymeAdapter() }) //let enzyme know to use react-16 to create its virtual DOM
+
+/**
+ * Factory function to create a ShallowWrapper for the App component
+ * @function setup
+ * @param {object} props - Component props specific to this setup
+ * @param {any} state - Initial state for setup
+ * @returns {ShallowWrapper}
+ */
+const setup = (props = {}, state = null) => {
+    return shallow(<App {...props} />)
+}
+
+/**
+ * Return ShallowWrapper containing nodes(s) with the given data-test value.
+ * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper to search within
+ * @param {string} val - value of data-set attribute for search
+ * @return {ShallowWrapper}
+ */
+const findByTestAttr = (wrapper, val) => {
+    return wrapper.find(`[data-test="${val}"]`)
+}
+
+test('renders without crashing', () => {
+    const wrapper = setup() // shallow rendering: render parent. but use placeholders
+    const appComponent = findByTestAttr(wrapper, 'component-app')
+    expect(appComponent.length).toBe(1)
+})
+
+test('renders increment button', () => {
+    const wrapper = setup()
+    const button = findByTestAttr(wrapper, 'increment-button')
+    expect(button.length).toBe(1)
+})
+
+test('renders counter display', () => {
+    const wrapper = setup()
+    const counterDisplay = findByTestAttr(wrapper, 'counter-display')
+    expect(counterDisplay.length).toBe(1)
+})
+
+test('counter starts at 0', () => {})
+
+test('clicking button increments counter display', () => {})
+
+test('', () => {})
